af5e312f7fde7af599a1c58eb03720eaf106f5d5
aftlah <altaf.fattah05@gmail.com>
2024-10-18 20:49:00 +0700

    refactor: delete command

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from 'next/server';
 
-// Fungsi handler untuk menangani request GET
 export async function GET() {
   return NextResponse.json({ message: 'Hello from Next.js API with TypeScript!' });
 }
 
-// Fungsi handler untuk request POST
 export async function POST(req: Request) {
   const body = await req.json();
   const { todo } = body;
