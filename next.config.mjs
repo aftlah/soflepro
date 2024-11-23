4868455bd389933cd72275845c9e49d8571a983b
aftlah <altaf.fattah05@gmail.com>
2024-11-23 14:59:23 +0700

    checkpoint

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,11 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: "export",
+// };
+
+// export default nextConfig;
